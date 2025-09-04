c8c309275d6c42f78862864243d2c15688e9389e
Abhi Kalakoti <abhikalakoti@gmail.com>
2025-09-04 14:08:58 +0100

    Added todo and project base structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./styles.css";
+
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+        this.priority = priority
+    }
+}
+
+class Project {
+    constructor(name) {
+        this.name = name
+        this.todos = []
+    }
+
+    createTodo(title, description, dueDate, priority) {
+        const todo = new Todo(title, description, dueDate, priority)
+        this.todos.push(todo)
+    }
+
+}
+
+const project = new Project("Home")
+project.createTodo("eat food", "nom", new Date("September 5, 2025"), "high")
+console.log(project.todos)
+
+
+
+
+
